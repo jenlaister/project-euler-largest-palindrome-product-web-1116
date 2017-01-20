d5f93d038a8d5bfcb796e2527f0f5ee8aceb8bfb
Jenny Lai <jenny.lai@flatironschool.com>
2017-01-19 23:03:23 -0500

    done

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,15 @@
 # Implement your procedural solution here!
+
+def palindrome?(number)
+  number.to_s == number.to_s.reverse
+end
+
+def largest_palindrome_product(range = 1..999)
+  array = []
+  range.each do |x|
+    range.each do |y|
+      array.push(x * y)
+    end
+  end
+  array.select { |n| palindrome?(n) }.max
+end
--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,22 @@
 # Implement your object-oriented solution here!
+class LargestPalindromeProduct
+
+attr_accessor :number
+
+def palindrome?(number)
+  number.to_s == number.to_s.reverse
+end
+
+def answer(range = 1..999)
+  array = []
+  range.each do |x|
+    range.each do |y|
+      array.push(x * y)
+    end
+  end
+  array.select { |n| palindrome?(n) }.max
+end
+
+
+
+end
